23918540f7f79d357ba282742b04364101c5669d
Yohannes <johnherma72b@gmail.com>
2024-03-11 09:30:22 -0900

    I change something

--- DateTime.py
+++ DateTime.py
@@ -1,5 +1,15 @@
+# import datetime
+# current_datetime = datetime.datetime.now()
+# print("Current Date and Time is: ")
+# print(current_datetime.strftime("%d-%m-%y"))
+# # print(current_datetime.strftime("%H:%M:S"))
+
+
+
 import datetime
-current_datetime = datetime.datetime.now()
-print("Current Date and Time is: ")
-print(current_datetime.strftime("%d-%m-%y"))
-print(current_datetime.strftime("%H:%M:S"))
+Date = datetime.datetime.now()
+print()
+print("Today: ",Date.strftime("%d-%m-%y"))
+Time = datetime.datetime.now()
+print()
+print("Time: " ,Time.strftime("%H:%M:%S"))
