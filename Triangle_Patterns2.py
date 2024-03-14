8f17484370014ca7fee4daa42dc95d829ea509b1
Yohannes <johnherma72b@gmail.com>
2024-03-13 21:22:16 -0900

    no change

--- Triangle_Patterns2.py
+++ Triangle_Patterns2.py
@@ -1,6 +1,6 @@
 import argparse
 def right_pyramid(n):
-    for raw in range(1, 1+n):
+    for raw in range(1, n+1):
         start_index = (n-raw)
         print(""*start_index, end = "")
         for star in range(raw):
